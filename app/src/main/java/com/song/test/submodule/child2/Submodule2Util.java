ef61ec5777e48b587506ecc5a4b872bad8257445
Banton <1039935805@qq.com>
2017-06-26 15:09:45 +0800

    init

---
+++ app/src/main/java/com/song/test/submodule/child2/Submodule2Util.java
@@ -0,0 +1,12 @@
+package com.song.test.submodule.child2;
+
+import android.util.Log;
+
+/**
+ * Created by songyawei on 2017/6/26.
+ */
+public class Submodule2Util {
+    public static void test() {
+        Log.e(Submodule2Util.class.getSimpleName(), "Submodule2Util");
+    }
+}
